ca66ad570f2d1047cd0d1a0b0fe8a530de7779c7
Ronabet <ronabet456@gmail.com>
2020-08-18 12:36:10 +0300

    refactor: server

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const cors = require('cors');
+const port = process.env.PORT || 80;
 
-
-var port = process.env.PORT || 80;
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('access-Control-Allow-Origin', '*');
-    next();
-});
-
-
+app.use(cors());
 app.use('/', express.static(__dirname + '/Front/dist/web-crawler-client'));
-
 
 http.listen(port, function(){
     console.log('listening on *: ' + port);
